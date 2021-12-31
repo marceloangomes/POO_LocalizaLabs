a707066bf90adeb4ed75b1459e1cd44c402088c6
marceloangomes <marcelogomes723@gmail.com>
2021-12-31 09:11:52 -0300

    Inicial

---
+++ ExemploPOO/Models/Pessoa.cs
@@ -0,0 +1,12 @@
+namespace ExemploPOO.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar(){
+            Console.WriteLine($"Olá, meu nome é {this.Nome} e tenho {this.Idade} anos.");
+        }
+    }
+}
---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+
+
+using ExemploPOO.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome="João";
+p1.Idade=30;
+
+p1.Apresentar();
+
+
+
